eaf71e868d0d5e0b1df1fc1b165306c531d2e628
Ahmed Jabareen <ahmed.jabareen1@gmail.com>
2022-11-06 12:59:42 +0200

    fix in script 2

--- HW2 Scripts/question2.sh
+++ HW2 Scripts/question2.sh
@@ -8,9 +8,15 @@ INPUT=$1
 
 if [ $# -lt 1 ];
 then    
-    echo "Error: there's no input. (one parameter is required)"
+    echo "Error: there's no input. (one integer is required)"
     exit 1
 fi
+
+re='^[0-9]+$'
+if ! [[ $INPUT =~ $re ]] ; then
+   echo "error: Input is not an integer" >&2; exit 1
+fi
+
 RESULT=$(($INPUT-32))
 RESULT=$((RESULT*5/9))
 
