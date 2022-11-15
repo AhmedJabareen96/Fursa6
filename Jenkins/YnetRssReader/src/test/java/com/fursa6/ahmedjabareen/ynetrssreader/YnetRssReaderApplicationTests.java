57a86c9b5eb52f73fc53d607a011a69a194f4f59
Ahmed Jabareen <ahmed.jabareen1@gmail.com>
2022-11-15 10:52:22 +0200

    hw3 init

---
+++ Jenkins/YnetRssReader/src/test/java/com/fursa6/ahmedjabareen/ynetrssreader/YnetRssReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fursa6.ahmedjabareen.ynetrssreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnetRssReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
